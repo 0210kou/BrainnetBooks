499783cf7facdf34d713b42e65e0b63cf12c9031
Owner <Owner@SS07>
2015-03-03 14:43:50 +0900

    2015/03/03 14:43

--- BrainnetBooks/Dao/BookDAO.java
+++ BrainnetBooks/Dao/BookDAO.java
@@ -3,81 +3,80 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  *
  * @author 白川
- * Bookテーブルに接続し、処理を担当するDAO
+ * bookに接続し、処理を担当するDAO
  *
  */
 public class BookDAO {
-	public Book findBook(Book book) {
+
+	public List<Book> findBook(){
 		Connection conn = null;
-		//Book book = null;
-		try{
-			//JDBCドライバを読み込む
-			Class.forName("com.mysql.jdbc.Driver");
-
-			//データベースに接続
-			conn = DriverManager.getConnection
-					("jdbc:mysql://192.168.137.0:3306/bn_books","bn_user","");
-
-			//Select文を準備
-			String sql = "select * from book "
-					+ "where book_name = ? or"
-					+ "where author_name = ?  or"
-					+ "where publisher_name = ? ";
-
-			//準備したSQL文をPreparedStatementインスタンスに渡す
-			PreparedStatement pStmt = conn.prepareStatement(sql);
-			pStmt.setString(1, book.getBook_name());
-			pStmt.setString(2, book.getAuthor_name());
-			pStmt.setString(3, book.getPublisher_name());
-
-
-			//select文を実行し、ResultSetインスタンスにSelect文の実行結果を取得
-			ResultSet rs = pStmt.executeQuery();
-
-			//そのBookを表すBookインスタンスを生成
-			if(rs.next()){
-				String isbn			= rs.getString("isbn");
-				String book_name = rs.getString("book_name");
-				String author_name = rs.getString("author_name");
-				String publisher_name = rs.getString("publisher_name");
-				int    price			= rs.getInt("price");
-				String  release_date = rs.getString("release_date");
-				int category_id = rs.getInt("category_id");
-				String  modify_datetime = rs.getString("modify_datetime");
-				String 	create_datetime = rs.getString("create_datetime");
-
-				 Book book2 = new Book(isbn,book_name,author_name,
-				 publisher_name, price, release_date,
-						 category_id, modify_datetime, create_datetime);
-
-			}
+		List<Book> bookList = new ArrayList<Book>();
 
 
 
+	try{
 
-		}catch(SQLException e){
-			e.printStackTrace();
-			return null;
-		}catch(ClassNotFoundException e){
-			e.printStackTrace();
-			return null;
-		}finally{
-			//データベース切断
-			if(conn != null){
-				try{
-					conn.close();
-				}catch(SQLException e){
-					e.printStackTrace();
-					return null;
+//		JDBCドライバを読み込む
+		Class.forName("com.mysql.jdbc.Driver");
 
-				}
+//		データベースへ接続
+		conn = DriverManager.getConnection
+				("jdbc:mysql://192.168.137.0:3306/bn_books","bn_user","");
+
+//		select文の準備
+		String sql = "select isbn, book_name from book";
+
+		//準備したSQL文をPreparedStatementインスタンスに渡す
+		PreparedStatement pStmt = conn.prepareStatement(sql);
+
+		//select文を実行し、ResultSetインスタンスにSelect文の実行結果を取得
+		ResultSet rs = pStmt.executeQuery();
+
+//		rsに格納されたレコードの内容をBookインスタンスに設定し、ArrayList
+	//	インスタンスに追加
+
+		while(rs.next()){
+			String isbn			= rs.getString("isbn");
+			String book_name = rs.getString("book_name");
+//			String author_name = rs.getString("author_name");
+//			String publisher_name = rs.getString("publisher_name");
+//			int    price			= rs.getInt("price");
+//			String  release_date = rs.getString("release_date");
+//			int category_id = rs.getInt("category_id");
+//			String  modify_datetime = rs.getString("modify_datetime");
+//			String 	create_datetime = rs.getString("create_datetime");
+
+//			インスタンスの生成
+			Book book = new Book(isbn,book_name);
+
+			bookList.add(book);
+		}
+
+
+	}catch(SQLException e){
+		e.printStackTrace();
+	}catch(ClassNotFoundException e){
+		e.printStackTrace();
+		return null;
+	}finally{
+		if(conn != null){
+			try{
+				conn.close();
+			}catch(SQLException e){
+				e.printStackTrace();
+				return null;
 			}
 		}
-		//検索結果をまたはnullを返す
-		return book2;
+
 	}
+	return bookList;
+
+}
 }
--- BrainnetBooks/src/TestBook.java
+++ BrainnetBooks/src/TestBook.java
@@ -1,13 +1,30 @@
-import static org.junit.Assert.*;
+import java.util.List;
 
 import org.junit.Test;
 
 
 public class TestBook {
 
-	@Test
-	public void test() {
-		fail("まだ実装されていません");
+//	(expected=IllegalArgumentException.class)
+	@Test public void test() {
+
+//		Bookインスタンスを生成
+		Book book = new Book();
+//		DAOインスタンスを生成
+		BookDAO dao = new BookDAO();
+//		fail("まだ実装されていません");
+	}
+	@Test public void test1(){
+
+		BookDAO dao = new BookDAO();
+		List<Book> bookList = dao.findBook();
+//		System.out.println(book.get(0));
+
+		for(Book bookBook : bookList){
+			System.out.println(bookBook.getIsbn());
+			System.out.println(bookBook.getBook_name());
+		}
+
 	}
 
 }
