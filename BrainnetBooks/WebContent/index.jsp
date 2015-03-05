47db6ef1d05b66c5655ad4ce3af6ebf929d64394
Owner <Owner@SS07>
2015-03-05 18:33:46 +0900

    2015年  3月  5日 木曜日 18:33:36

--- BrainnetBooks/WebContent/index.jsp
+++ BrainnetBooks/WebContent/index.jsp
@@ -10,38 +10,39 @@
 <body>
 <div id="headermenu" align="right">
 <p>
-<label for="namae" accesskey="n">ユーザーID：</label></div>
+<label for="namae" accesskey="n">ユーザーID：<%-- getUserID() --%></label></div>
 <p>
 
 <div id="" align="center" >
-<a href="URL" target="_self">書籍検索</a>
-　　　<a href="URL" target="_self">ショッピングカート </a>
-　　　<a href="URL" target="_self">注文履歴 </a>
-　　　<a href="URL" target="_self">ログイン</a><a href="URL" target="_self">/ログアウト </a>
+<a href="URL" >書籍検索</a>
+　　　<a href="URL">ショッピングカート </a>
+　　　<a href="URL" >注文履歴 </a>
+　　　<a href="URL" >ログイン</a>　<a href="URL" >/ログアウト </a>
 </div>
 <br>
 <br>
 <br>
 <div id= "keyword" align="center">
-■検索キーワード<br>
+■検索キーワード<br><p>
+<form action="/BrainnetBooks/FindBook" method="post">
 　　　書籍　　　　　　<input type="text" name="bookName"><br>
 <p>
 　　　著者　　　　　　<input type="text" name="authorName"><br>
 <p>
  　出版社　　　　　　 <input type="text" name="publisherName"><br>
 <p>
- カテゴリー　　　　　 <input type="text" name="categoryName">
-<p>
-<input type="submit" value="検索">
+ カテゴリー　　　　　 <input type="text" name="categoryName"><br>
+ <p>
+　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　<input type="submit" value="検索">
 <br>
+</form>
 </div>
 <br>
 <p>
 
+<%-- for検索条件ヒットした回数文表示 --%>
 <table align="center"><caption>■検索結果一覧</caption>
 <tr>
-
-
 	<th>書籍情報</th>
 
 	<th>著者名</th>
@@ -50,33 +51,17 @@
 	<th>購入希望</th>
 	</tr>
 <tr>
-	<th>-</th>
-	<td>-</td>
-	<td>-</td>
-	<td>-</td>
-	<td><input type="text"size=1 name=""><input type="submit" value="追加"></td>
+	<th><%-- get書籍名、ISBM,発売日、カテゴリ名() --%></th>
+	<td><%-- get著者名() --%> 著</td>
+	<td><%-- get出版社() --%></td>
+	<td><%-- get税抜価格() --%></td>
+	<td><form action="" method="Post"><input type="text"size=1 name="">
+	<input type="submit" value="追加"></form></td>
+
 </tr>
-<tr>
-	<th>-</th>
-	<td>-</td>
-	<td>-</td>
-	<td>-</td>
-	<td><input type="text" size=1 name="" ><input type="submit" value="追加"></td>
-</tr>
-<tr>
-	<th>-</th>
-	<td>-</td>
-	<td>-</td>
-	<td>-</td>
-	<td><input type="text" size=1 name=""><input type="submit" value="追加"></td>
-</tr>
-<tr>
-	<th>-</th>
-	<td>-</td>
-	<td>-</td>
-	<td>-</td>
-	<td><input type="text"size=1 name=""><input type="submit" value="追加"></td>
-</tr>
+
 </table>
+
+
 </body>
 </html>
--- BrainnetBooks/src/Dao/BookDAO.java
+++ BrainnetBooks/src/Dao/BookDAO.java
@@ -5,8 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
 
 import Entity.Book;
 
@@ -14,73 +13,176 @@ import Entity.Book;
 /**
  *
  * @author 白川
- * bookに接続し、処理を担当するDAO
+ * Bookテーブルに接続し、処理を担当するDAO
  *
  */
 public class BookDAO {
-
-	public List<Book> findBook(){
+	public Book findBook(String a, String b, String c, String d) {
 		Connection conn = null;
-		List<Book> bookList = new ArrayList<Book>();
+		Book book = null;
+
+		try{
+			//JDBCドライバを読み込む
+			Class.forName("com.mysql.jdbc.Driver");
+
+			//データベースに接続
+			conn = DriverManager.getConnection
+					("jdbc:mysql://192.168.137.0:3306/bn_books","bn_user","");
+
+			//Select文を準備(5.1検索結果には書籍名、著者名、出版社名、税抜き価格)を表示する
+			String sql = "select * from book "
+					+ "where book_name like ? and" //primary
+					+ " author_name like ? and"		//primary book_name,author_name,publisher_name,price
+					+ " publisher_name = ? and" //secondary
+					+ " category_id = ? "; //secondary
+//
+
+
+			//準備したSQL文をPreparedStatementインスタンスに渡す
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			//category_idをshortにキャスト
+			short e = Short.parseShort(d);
+
+			//エスケープをreplaceAllにて実行
+			a = a.replaceAll("%","\\\\%").replaceAll("_","\\\\_");
+			b = b.replaceAll("%","\\\\%").replaceAll("_","\\\\_");
+			c = c.replaceAll("%","\\\\%").replaceAll("_","\\\\_");
+			d = d.replaceAll("%","\\\\%").replaceAll("_","\\\\_");
+//			System.out.println(sql);
+			pStmt.setString(1, "%" + a + "%");
+			pStmt.setString(2, "%" + b + "%");
+			pStmt.setString(3,c);
+			pStmt.setShort(4,e);
+//			System.out.println(pStmt);
+
+			//select文を実行し、ResultSetインスタンスにSelect文の実行結果を取得
+			ResultSet rs = pStmt.executeQuery();
+
+			//そのBookを表すBookインスタンスを生成
+			while(rs.next()){
+				String isbn			= rs.getString("isbn");
+				String book_name = rs.getString("book_name");
+				String author_name = rs.getString("author_name");
+				String publisher_name = rs.getString("publisher_name");
+				short    price			= rs.getShort("price");
+				String  release_date = rs.getString("release_date");
+				short category_id = rs.getShort("category_id");
+				Date  modify_datetime = rs.getDate("modify_datetime");
+				Date 	create_datetime = rs.getDate("create_datetime");
+
+				 book = new Book(isbn,book_name,author_name,publisher_name,
+						 price, release_date,
+						category_id, modify_datetime,create_datetime ); //
+				 System.out.println(book.toString());
+			}
+//
 
 
 
-	try{
 
-//		JDBCドライバを読み込む
-		Class.forName("com.mysql.jdbc.Driver");
+		}catch(SQLException e){
+			e.printStackTrace();
+			return null;
+		}catch(ClassNotFoundException e){
+			e.printStackTrace();
+			return null;
+		}finally{
+			//データベース切断
+			if(conn != null){
+				try{
+					conn.close();
 
-//		データベースへ接続
-		conn = DriverManager.getConnection
-				("jdbc:mysql://192.168.137.0:3306/bn_books","bn_user","");
+				}catch(SQLException e){
+					e.printStackTrace();
+					return null;
 
-//		select文の準備
-		String sql = "select isbn, book_name from book";
-
-		//準備したSQL文をPreparedStatementインスタンスに渡す
-		PreparedStatement pStmt = conn.prepareStatement(sql);
-
-		//select文を実行し、ResultSetインスタンスにSelect文の実行結果を取得
-		ResultSet rs = pStmt.executeQuery();
-
-//		rsに格納されたレコードの内容をBookインスタンスに設定し、ArrayList
-	//	インスタンスに追加
-
-		while(rs.next()){
-			String isbn			= rs.getString("isbn");
-			String book_name = rs.getString("book_name");
-//			String author_name = rs.getString("author_name");
-//			String publisher_name = rs.getString("publisher_name");
-//			short    price			= rs.getShort("price");
-//			String  release_date = rs.getString("release_date");
-//			int category_id = rs.getInt("category_id");
-//			Date  modify_datetime = rs.getDate("modify_datetime");
-//			Date 	create_datetime = rs.getDate("create_datetime");
-
-//			インスタンスの生成
-			Book book = new Book(isbn,book_name);
-
-			bookList.add(book);
-		}
-
-
-	}catch(SQLException e){
-		e.printStackTrace();
-	}catch(ClassNotFoundException e){
-		e.printStackTrace();
-		return null;
-	}finally{
-		if(conn != null){
-			try{
-				conn.close();
-			}catch(SQLException e){
-				e.printStackTrace();
-				return null;
+				}
 			}
 		}
-
+		//検索結果をまたはnullを返す
+		return book;
 	}
-	return bookList;
+/**
+ * 	insert
+ *
+ */
+	public Book additionBook(String a, String b,
+			String c, String d, short e, String f,short g){
+		Connection conn = null;
+		Book book = null;
+		try{
+			//JDBCドライバを読み込む
+			Class.forName("com.mysql.jdbc.Driver");
 
-}
+			//データベースに接続
+			conn = DriverManager.getConnection
+					("jdbc:mysql://192.168.137.0:3306/bn_books","bn_user","");
+
+			//Select文を準備
+			String insert = "insert into book(isbn,book_name,author_name,"
+					+ "publisher_name,price,release_date,category_id) "
+					+ " values(?, ?, ?, ?, ?, ?, ?)";
+
+			//準備したSQL文をPreparedStatementインスタンスに渡す
+			PreparedStatement pStmt = conn.prepareStatement(insert);
+			pStmt.setString(1, a);
+			pStmt.setString(2, b);
+			pStmt.setString(3, c);
+			pStmt.setString(4, d);
+			pStmt.setShort(5, e);
+			pStmt.setString(6, f);
+			pStmt.setShort(7, g);
+//			System.out.println(insert);
+
+
+			//insert文を実行
+			pStmt.executeUpdate(insert);
+//			System.out.println(pStmt);
+			/*
+			ResultSet rs = pStmt.executeQuery("SELECT * FROM book");
+
+			//そのBookを表すBookインスタンスを生成
+			while(rs.next()){
+				String isbn			= rs.getString("isbn");
+				String book_name = rs.getString("book_name");
+				String author_name = rs.getString("author_name");
+				String publisher_name = rs.getString("publisher_name");
+				short    price			= rs.getShort("price");
+				String  release_date = rs.getString("release_date");
+				short category_id = rs.getShort("category_id");
+				Date  modify_datetime = rs.getDate("modify_datetime");
+				Date 	create_datetime = rs.getDate("create_datetime");
+
+				book = new Book(isbn,book_name,author_name,publisher_name,
+						 price, release_date,
+						category_id, modify_datetime,create_datetime );
+
+			}*/
+
+
+
+
+		}catch(SQLException ee){
+			ee.printStackTrace();
+			return null;
+		}catch(ClassNotFoundException ee){
+			ee.printStackTrace();
+			return null;
+		}finally{
+			//データベース切断
+			if(conn != null){
+				try{
+					conn.close();
+
+				}catch(SQLException ee){
+					ee.printStackTrace();
+					return null;
+
+				}
+			}
+		}
+		//検索結果をまたはnullを返す
+				return book;
+	}
 }
