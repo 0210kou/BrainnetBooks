a1c30746199d727ab67e0c94c70e320da404b90f
Owner <Owner@SS07>
2015-03-05 14:01:29 +0900

    2015年  3月  5日 木曜日 14:01:20

--- BrainnetBooks/src/Dao/TestBook.java
+++ BrainnetBooks/src/Dao/TestBook.java
@@ -1,5 +1,7 @@
 package Dao;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 import Entity.Book;
@@ -7,92 +9,87 @@ import Entity.Book;
 public class TestBook {
 
 
-
-
+	//Bookインスタンス生成
+	Book book = new Book();
+//	findBook()を持っているclassのインスタンスを生成
 	BookDAOTest test = new BookDAOTest();
+
 	StringBuilder sb = new StringBuilder();
 
 
-/*
+
 	@Test public void test() {
-		StringBuilder sb = new StringBuilder();
-		Book book = test.findBook("Head First Java","Kathy Serra");
-		sb.append(book.getIsbn()+ "\n");
-		sb.append(book.getBook_name()+ "\n");
-		sb.append(book.getAuthor_name()+ "\n");
-		sb.append(book.getPublisher_name()+ "\n");
-		sb.append(book.getPrice()+ "\n");
-		sb.append(book.getRelease_date()+ "\n");
-		sb.append(book.getCategory_id()+ "\n");
-		sb.append(book.getModify_datetime()+ "\n");
-		sb.append(book.getCreate_datetime()+ "\n");
-		sb.append("\n");
-		System.out.println(sb.toString());
-	}*/
+
+		Book book = test.findBook("First Java","Serra","ILLY","3");
+//		第一引数と第二引数が同じ値であるか判別します。違う場合はfalseになります。
+					assertEquals("First Java", book.getBook_name());
+					assertEquals("Serra", book.getAuthor_name());
+					assertEquals("ILLY", book.getPublisher_name());
+					assertEquals(3, book.getCategory_id()); //shor型なの失敗する
+
+
+	}
 	@Test public void test1(){
 //		部分一致
 
-		Book book3 = test.findBook( "st","rra","O'REILLY","1");
-//		sb.append("test1");
-//		sb.append("\n");
-//		sb.append(book3.getIsbn()+ "\n");
-//		sb.append(book3.getBook_name()+ "\n");
-//		sb.append(book3.getAuthor_name()+ "\n");
-//		sb.append(book3.getPublisher_name()+ "\n");
-//		sb.append(book3.getPrice()+ "\n");
-//		sb.append(book3.getRelease_date()+ "\n");
-//		sb.append(book3.getCategory_id()+ "\n");
-//		sb.append(book3.getModify_datetime()+ "\n");
-//		sb.append(book3.getCreate_datetime()+ "\n");
-//		sb.append("\n");
-//
-//		System.out.println(sb.toString());
+		Book book1 = test.findBook( "st","rra","O'REILLY","1");
+
+		Book book2 = test.findBook("st","rra","O'REILLY","1");
+		//Object1とObject2が同じObjectを参照していないか判定する同じオブジェクトの場合
+//		falseを返す
+					assertNotSame(book1, book2);
 
 
-	}/*
+
+
+
+
+
+
+
+
+
+
+	}
 	@Test public void Test2(){
-		StringBuilder sb = new StringBuilder();
 
-	Book book = test.findBook("Head First Java","Kathy Serra");
-	sb.append(book.getIsbn()+ "\n");
-	sb.append(book.getBook_name()+ "\n");
-	sb.append(book.getAuthor_name()+ "\n");
-	sb.append(book.getPublisher_name()+ "\n");
-	sb.append(book.getPrice()+ "\n");
-	sb.append(book.getRelease_date()+ "\n");
-	sb.append(book.getCategory_id()+ "\n");
-	sb.append(book.getModify_datetime()+ "\n");
-	sb.append(book.getCreate_datetime()+ "\n");
-	sb.append("\n");
+		Book book3 = test.findBook("st","rra","O'REILLY","1");
+		Book book4 = new Book();
 
-	System.out.println(sb.toString());
+		//引数(Object)がNullでないか判定する
+				assertNotNull(book3);//引数(Object)がNullでないか判定する
+
+//		上記と同様に振舞うが、Nullだった場合には第一引数のStringを返す
+				assertNotNull("Nullになっている",book4.getCategory_id());
 
 
 
-
-
-
-
-	}*//*
+	}
 	@Test public void Test3(){
 
-//		insert
-		Book book4 = test.additionBook("3", "java入門", "中山", "インプレス",
-				(short)2800, "2014-08-11", (short)5);
-		sb.append(book4.getIsbn()+ "\n");
-		sb.append(book4.getBook_name()+ "\n");
-		sb.append(book4.getAuthor_name()+ "\n");
-		sb.append(book4.getPublisher_name()+ "\n");
-		sb.append(book4.getPrice()+ "\n");
-		sb.append(book4.getRelease_date()+ "\n");
-		sb.append(book4.getCategory_id()+ "\n");
-		sb.append(book4.getModify_datetime()+ "\n");
-		sb.append(book4.getCreate_datetime()+ "\n");
-		sb.append("\n");
+		Book book5 = new Book();
+		Book book6 = test.findBook("st","rra","O'REILLY","1");
 
-		System.out.println(sb.toString());
 
-    }*/
+				assertNotEquals(book5, book6);
+
+
+
+
+
+    }
+	@Test public void Test4(){
+
+		Book book7 = test.findBook("First Java","Serra","ILLY","3");
+//		第二引数がnullでないことを検証。
+		assertNotNull(book7.getCreate_datetime());
+
+	}
+	@Test public void Test5(){
+
+
+	}
+
 
 }
 
